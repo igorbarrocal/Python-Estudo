8a48d651ada586e2b9e9bdd7953a7131592546b3
igorbarrocal <igorbarrocal@hotmail.com>
2026-03-25 15:34:18 -0300

    refactor app.py to modularize functions for displaying program name and options

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
-
-print("""
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -10,26 +10,36 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
-##print(f'Você escolheu a opção {opcao_escolhida}')
-# opcao_escolhida = int(opcao_escolhida) ##converte a opção escolhida para inteiro, caso contrário, exibe mensagem de opção inválida
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('cls')
     print('Encerrando o aplicativo. Até logo!\n')
 
 
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    ##print(f'Você escolheu a opção {opcao_escolhida}')
+    # opcao_escolhida = int(opcao_escolhida) ##converte a opção escolhida para inteiro, caso contrário, exibe mensagem de opção inválida
 
-if opcao_escolhida == 1:
-    print('Opção de cadastrar restaurante selecionada')
-elif opcao_escolhida == 2:
-    print('Opção de listar restaurantes selecionada') ## o elif é usado para verificar cada opção escolhida, caso contrário, exibe mensagem de opção inválida
-elif opcao_escolhida == 3:
-    print('Opção de ativar restaurante selecionada')  ##if para cada opção escolhida, caso contrário, exibe mensagem de opção inválida                                                        
-else:
-    finalizar_app()
+    if opcao_escolhida == 1:
+        print('Opção de cadastrar restaurante selecionada')
+    elif opcao_escolhida == 2:
+        print('Opção de listar restaurantes selecionada') ## o elif é usado para verificar cada opção escolhida, caso contrário, exibe mensagem de opção inválida
+    elif opcao_escolhida == 3:
+        print('Opção de ativar restaurante selecionada')  ##if para cada opção escolhida, caso contrário, exibe mensagem de opção inválida                                                        
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == "__main__":
+    main()
