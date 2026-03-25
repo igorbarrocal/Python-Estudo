99868789a3de77a42dd72f7476d671df79e190eb
igorbarrocal <igorbarrocal@hotmail.com>
2026-03-25 15:17:18 -0300

    add menu options and input handling to app.py

---
+++ Exercicio/exercicio-1.py
@@ -0,0 +1,15 @@
+### 1 - Imprima a frase: Python na Escola de Programação da Alura.
+
+nome = 'Alura'
+idade = input('Qual é a sua idade:  ')
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+### 2 - Imprima o menu abaixo:
+
+print('\nA','L','U','R','A\n',sep='\n')
+
+## 3 -Para imprimir o valor de pi arredondado, temos algumas possíveis abordagens:
+
+pi = 3.14159
+print(f'Valor de pi arredondado: {pi:.2f}')
--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-print('\nSabor Express\n') 
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurantes')
-print('3. Ativar Restaurante')
+
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input ('Escolha uma opção: ')
-print(f'Você escolheu a opção: {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+##print(f'Você escolheu a opção {opcao_escolhida}')
+# opcao_escolhida = int(opcao_escolhida) ##converte a opção escolhida para inteiro, caso contrário, exibe mensagem de opção inválida
 
+if opcao_escolhida == 1:
+    print('Opção de cadastrar restaurante selecionada')
+elif opcao_escolhida == 2:
+    print('Opção de listar restaurantes selecionada') ## o elif é usado para verificar cada opção escolhida, caso contrário, exibe mensagem de opção inválida
+elif opcao_escolhida == 3:
+    print('Opção de ativar restaurante selecionada')  ##if para cada opção escolhida, caso contrário, exibe mensagem de opção inválida
+                                                        
+elif opcao_escolhida == 4:
+    print('Opção de sair selecionada')
+else:
+    print('Opção inválida!')
