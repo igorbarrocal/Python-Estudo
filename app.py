0324af223e14a30a853ebe24752c6a595a421b52
igorbarrocal <igorbarrocal@hotmail.com>
2026-03-25 15:23:43 -0300

    refactor app.py to improve option handling and add application exit functionality

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 
 
 print("""
@@ -18,14 +19,17 @@ opcao_escolhida = int(input('Escolha uma opção: '))
 ##print(f'Você escolheu a opção {opcao_escolhida}')
 # opcao_escolhida = int(opcao_escolhida) ##converte a opção escolhida para inteiro, caso contrário, exibe mensagem de opção inválida
 
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando o aplicativo. Até logo!\n')
+
+
+
 if opcao_escolhida == 1:
     print('Opção de cadastrar restaurante selecionada')
 elif opcao_escolhida == 2:
     print('Opção de listar restaurantes selecionada') ## o elif é usado para verificar cada opção escolhida, caso contrário, exibe mensagem de opção inválida
 elif opcao_escolhida == 3:
-    print('Opção de ativar restaurante selecionada')  ##if para cada opção escolhida, caso contrário, exibe mensagem de opção inválida
-                                                        
-elif opcao_escolhida == 4:
-    print('Opção de sair selecionada')
+    print('Opção de ativar restaurante selecionada')  ##if para cada opção escolhida, caso contrário, exibe mensagem de opção inválida                                                        
 else:
-    print('Opção inválida!')
+    finalizar_app()
