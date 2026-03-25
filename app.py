980c9b1cd7d3ee515978beac6a7d965d5e66bff6
igorbarrocal <igorbarrocal@hotmail.com>
2026-03-25 14:57:43 -0300

    fix formatting in app.py to ensure proper output display

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-print("Sabor Express\n") 
+print('\nSabor Express\n') 
 
 print('1. Cadastrar Restaurante')
 print('2. Listar Restaurantes')
 print('3. Ativar Restaurante')
-print('4. Sair')
+print('4. Sair\n')
+
+opcao_escolhida = input ('Escolha uma opção: ')
+print(f'Você escolheu a opção: {opcao_escolhida}')
+
